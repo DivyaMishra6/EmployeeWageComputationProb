a72a40cdf81d8d3ecf9898ddc8c956dba8bfb58f
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-10 18:24:43 +0530

    Employee Present or Absent

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EmployeeWagesProblem
+{
+   class Program
+    {
+
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage Problem");
+
+
+            // EmployeeCheck.EmployeeAttendence();
+            //CalculateEmpWage.EmployeeWages();
+            // AddPartTimeWage_UC3.Wages( EMP_RATE_PER_HOUR);
+            EmpWageUsingCase_UC4.Case();
+        }
+    }
+
+
+}
